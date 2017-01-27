b1b574dee204d40e9616cdcd8128a8ffc66c6487
Jon-Eric Cook <jec@sonic.net>
2017-01-27 11:04:29 -0800

    Add files via upload

---
+++ Activity - JS Functions/js_functions.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Jon-Eric Cook on 1/23/2017.
+ */
+console.log(double(2));
+
+function double (x)
+{
+    return 2*x;
+}
+
+console.log(triple(3));
+
+var triple = function (x)
+{
+    return 3*x;
+}
