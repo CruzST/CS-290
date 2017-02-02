13027b9f3d4cd69fa5492abc219fc70e95c03c91
Jon-Eric Cook <jec@sonic.net>
2017-02-01 22:19:08 -0800

    finished the closure exercise

---
+++ Exercise - Fixing Closure Loop/fix_closure_loop.js
@@ -0,0 +1,26 @@
+function buildList(list) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+        (function(x) {
+            var item = 'item' + list[x];
+            result.push(function() {
+                alert(item + ' ' + list[x])
+            });
+
+        })(i);
+    }
+    return result;
+}
+
+function testList() {
+    var fnlist = buildList([1, 2, 3]);
+    // using j only to help prevent confusion - could use i
+    for (var j = 0; j < fnlist.length; j++) {
+        fnlist[j]();
+    }
+}
+
+testList();
+
+// I a lot of spent time review the article below and the above code reflects it.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
