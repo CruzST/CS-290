6f898fd6f7ead9622cf9bbd6bac5e51d9526fc3d
Jon-Eric Cook <jec@sonic.net>
2017-02-21 14:06:55 -0800

    Finished it
    Worked through it with the ta

--- HW-Assignment-GET-and-POST /get-post.js
+++ HW-Assignment-GET-and-POST /get-post.js
@@ -1,40 +1,59 @@
-var express = require('express');
+// location of live website http://flip3.engr.oregonstate.edu:3948
 
+var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 3948);
 
+// when the users goes to the home page, they are served home.handlebars
 app.get('/',function(req,res){
   res.render('home');
 });
 
+// when the user makes a get request, an array stores all the
+// items in the query
 app.get('/get',function(req,res){
-  var keyVals = [];
+  var keyValsQ = [];
   for (var item in req.query){
-    keyVals.push({'key':item,'val':req.query[item]})
+    keyValsQ.push({'key':item,'val':req.query[item]})
   }
+
+  // the array of items is put into an object
   var content = {};
-  content.data = keyVals;
+  content.Qdata = keyValsQ;
+
+  // a call type is indicated
   content.callType = "GET";
+
+  // get-post.handlebars is served
   res.render('get-or-post', content);
 });
 
+// when the user makes a post request, an array stores all the
+// items in the body
 app.post('/post', function(req,res){
-  var keyVals = [];
+  var keyValsB = [];
   for (var item in req.body){
-    keyVals.push({'key':item,'val':req.body[item]})
+    keyValsB.push({'key':item,'val':req.body[item]})
   }
-  console.log(keyVals);
+
+// when the user makes a post request, an array stores all the
+// items in the query
+  var keyValsQ = [];
+  for (var item in req.query){
+    keyValsQ.push({'key':item,'val':req.query[item]})
+  }
+  
   console.log(req.body);
+  console.log(req.query);
   var content = {};
-  content.data = keyVals;
+  content.Bdata = keyValsB;
+  content.Qdata = keyValsQ;
   content.callType = "POST";
   res.render('get-or-post', content);
 });
